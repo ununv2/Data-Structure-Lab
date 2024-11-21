76fa996c6b3596731869463f0f13db0d789def9b
ununnn__ <ununxuyy@gmail.com>
2024-11-21 17:24:14 +0700

    implement Treap class

--- Lab_16_2/src/Treap.java
+++ Lab_16_2/src/Treap.java
@@ -1,110 +1,114 @@
 public class Treap {
-    TreapNode root;
-    int size;
-
-    // Constructor with no parameters
+	TreapNode root;
+	int size;
     public Treap() {
-        root = null;
-        size = 0;
+    	root = null;
+    	size = 0;
     }
-
-    // Insert method for inserting bstValue and heapValue
     public TreapNode insert(int v, int h) {
-        // First, check if the (v, h) pair already exists.
-        TreapNode foundNode = find(v, root);
-        if (foundNode != null) {
-            return null; // Node already exists
-        }
-
-        // Insert the node as if in a BST.
-        TreapNode newNode = new TreapNode(v, h);
-        root = bstInsert(root, newNode);
-        size++;
-
-        // Rotate the node up the tree to maintain heap properties.
-        root = rotateUp(root, newNode);
+    	// if it exists, do nothing, return null ( find )
+    	// else insert new node that contains these values ( insert )
+    	// use heap value to rotate it up the tree [ small to large ] ( rotate AVL? )
+    	// return node that we added
+    	if(isExist(v,h)) {
+    		return null;
+    	}
+    	TreapNode newNode = new TreapNode(v,h);
+    	root = insertBST(root, newNode);
+    	size++;
+    	root = rebalance(newNode);
         return newNode;
+ 
+    	
     }
-
-    // Helper method to perform BST insertion
-    private TreapNode bstInsert(TreapNode current, TreapNode nodeToInsert) {
-        if (current == null) {
-            return nodeToInsert;
-        }
-        if (nodeToInsert.bstValue < current.bstValue) {
-            current.left = bstInsert(current.left, nodeToInsert);
-            current.left.parent = current;
-        } else {
-            current.right = bstInsert(current.right, nodeToInsert);
-            current.right.parent = current;
-        }
-        return current;
+    
+    public TreapNode insertBST(TreapNode n, TreapNode toInsert) {
+    	if(n == null) {
+    		return toInsert;
+    	}
+    	if(toInsert.bstValue < n.bstValue) {
+    		n.left = insertBST(n.left, toInsert);
+    		n.left.parent = n;
+    	}else {
+    		n.right = insertBST(n.right, toInsert);
+    		n.right.parent = n;
+    	}
+    	return n;
     }
+    
+    public boolean isExist(int v, int h) { 
+		TreapNode temp = root;
 
-    // Helper method to rotate the node up to maintain heap properties
-    private TreapNode rotateUp(TreapNode current, TreapNode nodeToRotate) {
-        while (nodeToRotate.parent != null && nodeToRotate.heapValue < nodeToRotate.parent.heapValue) {
-            TreapNode parent = nodeToRotate.parent;
-            if (parent.left == nodeToRotate) {
-                current = rotateRight(parent);
+		while (temp != null){
+			if(temp.bstValue == v && temp.heapValue == h) {
+				return true;
+			}
+			else if (v < temp.bstValue) {
+					temp = temp.left;
+			} else {
+				temp = temp.right;
+			}
+		}
+		return false;
+	}
+    private TreapNode rebalance(TreapNode nodeToRebalance) {
+        while (nodeToRebalance.parent != null && nodeToRebalance.heapValue < nodeToRebalance.parent.heapValue) {
+            TreapNode parent = nodeToRebalance.parent;
+            if (parent.left == nodeToRebalance) {
+                nodeToRebalance = rotateRight(parent);
             } else {
-                current = rotateLeft(parent);
+                nodeToRebalance = rotateLeft(parent);
             }
         }
-        return current;
+        return root;
     }
 
     // Rotate right around the given node
     private TreapNode rotateRight(TreapNode node) {
         TreapNode newRoot = node.left;
-        node.left = newRoot.right;
-        if (newRoot.right != null) {
-            newRoot.right.parent = node;
+        TreapNode lr = newRoot.right;
+        node.left = lr;
+        if (lr != null) {
+            lr.parent = node;
         }
         newRoot.right = node;
         newRoot.parent = node.parent;
-        if (node.parent == null) {
-            root = newRoot;
-        } else if (node.parent.left == node) {
-            node.parent.left = newRoot;
-        } else {
-            node.parent.right = newRoot;
-        }
         node.parent = newRoot;
+        if (newRoot.parent == null) {
+            root = newRoot;
+        } else if (newRoot.parent.left == node) {
+            newRoot.parent.left = newRoot;
+        } else {
+            newRoot.parent.right = newRoot;
+        }
         return newRoot;
     }
 
     // Rotate left around the given node
     private TreapNode rotateLeft(TreapNode node) {
         TreapNode newRoot = node.right;
-        node.right = newRoot.left;
-        if (newRoot.left != null) {
-            newRoot.left.parent = node;
+        TreapNode rl = newRoot.left;
+        node.right = rl;
+        if (rl != null) {
+            rl.parent = node;
         }
         newRoot.left = node;
         newRoot.parent = node.parent;
-        if (node.parent == null) {
-            root = newRoot;
-        } else if (node.parent.left == node) {
-            node.parent.left = newRoot;
-        } else {
-            node.parent.right = newRoot;
-        }
         node.parent = newRoot;
+        if (newRoot.parent == null) {
+            root = newRoot;
+        } else if (newRoot.parent.left == node) {
+            newRoot.parent.left = newRoot;
+        } else {
+            newRoot.parent.right = newRoot;
+        }
         return newRoot;
     }
+    
 
-    // Helper method to find a node with the given bstValue
-    private TreapNode find(int v, TreapNode current) {
-        if (current == null) {
-            return null;
-        }
-        if (v == current.bstValue) {
-            return current;
-        } else if (v < current.bstValue) {
-            return find(v, current.left);
-        } else {
-            return find(v, current.right);
-        }
-    }
+
+
 }
+
+    
+    
